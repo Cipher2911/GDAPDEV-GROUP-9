ad218d2b8d499fc6df13a46567a2cf58fed477f2
fandom-random <miguel.r.ileto@gmail.com>
2026-03-09 14:25:49 +0800

    Added Models (V1)
    lemme know of any suggestions and/or changes

---
+++ models/Reservation.js
@@ -0,0 +1,17 @@
+import { ObjectId } from "bson";
+
+const mongoose = require("mongoose");
+const { timeStamp } = require("node:console");
+
+const reserveSchema = new mongoose.Schema(
+    {
+        userReserved: { type: ObjectId, required: true },
+        roomReserved: { type: ObjectId, required: true },
+        dateReserved: { type: Date, required: true },
+        dateMade: { type: Date, required: true },
+        status: { type: String, default: "Active" }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Reservation", reserveSchema);
